5291f2036e6e8f7f6b4081235b8eaff87fe629d2
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2019-12-25 20:46:45 -0300

    Remove Https Route

--- DattingApp.API/Startup.cs
+++ DattingApp.API/Startup.cs
@@ -36,7 +36,7 @@ namespace DattingApp.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
