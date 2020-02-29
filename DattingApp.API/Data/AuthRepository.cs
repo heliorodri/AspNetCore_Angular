7fdf051d87c9c22afbc0746c7b81e79cafebb7ae
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2020-02-29 13:58:06 -0300

    Update Register method and create CreatePasswordHash method

--- DattingApp.API/Data/AuthRepository.cs
+++ DattingApp.API/Data/AuthRepository.cs
@@ -10,20 +10,38 @@ namespace DattingApp.API.Data
         {
             this._context = context;
         }
-        
+
         public Task<User> Login(string username, string password)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.passwordHash = passwordHash;
+            user.passwordSalt = passwordSalt;
+
+            await this._context.User.AddAsync(user);
+            await this._context.SaveChangesAsync();
+
+            return user;
         }
 
         public Task<bool> UserExistis(string username)
         {
             throw new System.NotImplementedException();
+        }
+
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
