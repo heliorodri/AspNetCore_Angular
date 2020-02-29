a38f84eac0b01d2e97a5e9dba8ca767ee745a5fc
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2020-02-29 14:17:48 -0300

    Update Login method and create VerifyPasswordHash method

--- DattingApp.API/Data/AuthRepository.cs
+++ DattingApp.API/Data/AuthRepository.cs
@@ -1,5 +1,7 @@
 using DattingApp.API.Models;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace DattingApp.API.Data
 {
@@ -11,9 +13,30 @@ namespace DattingApp.API.Data
             this._context = context;
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await this._context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+            
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i=0; i < computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
@@ -21,10 +44,10 @@ namespace DattingApp.API.Data
             byte[] passwordHash, passwordSalt;
             CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
-            user.passwordHash = passwordHash;
-            user.passwordSalt = passwordSalt;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
 
-            await this._context.User.AddAsync(user);
+            await this._context.Users.AddAsync(user);
             await this._context.SaveChangesAsync();
 
             return user;
@@ -42,6 +65,11 @@ namespace DattingApp.API.Data
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
