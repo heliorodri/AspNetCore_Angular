793e5d64fe88ace3b2812d8255bb1b0f9bd55163
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2020-02-29 13:44:39 -0300

    Create AuthRepository.cs

---
+++ DattingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,29 @@
+using DattingApp.API.Models;
+using System.Threading.Tasks;
+
+namespace DattingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            this._context = context;
+        }
+        
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UserExistis(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
