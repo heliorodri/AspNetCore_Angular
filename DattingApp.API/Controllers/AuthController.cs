eae8923c5f2b8ddb920b74891177dc6e0a22356a
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2020-02-29 14:50:17 -0300

    Using DTOs
    Creating "UserForRegisterDto" and changing the "Register" method to support it

--- DattingApp.API/Controllers/AuthController.cs
+++ DattingApp.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DattingApp.API.Data;
+using DattingApp.API.Dtos;
 using DattingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,16 +15,16 @@ namespace DattingApp.API.Controllers
         public AuthController(IAuthRepository repo){this._repo = repo;}
 
         [HttpPost("Register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             //Validate request
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if (await this._repo.UserExists(username))
+            if (await this._repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Usuário já existe");
             
-            var userToCreate = new User{Username = username};
-            var createdUser = await this._repo.Register(userToCreate, password);
+            var userToCreate = new User{Username = userForRegisterDto.Username};
+            var createdUser = await this._repo.Register(userToCreate, userForRegisterDto.Password);
 
             //change to CreatedAtRoute when the method is ready
             return StatusCode(201);
