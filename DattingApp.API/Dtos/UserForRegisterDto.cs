001cf2571e055caee8cad0ffb1b886f55a89fe07
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2020-04-06 15:20:24 -0300

    Validating UserDto/API
    Creating some validating for UserName and Password. Not allowing user to leave it null

--- DattingApp.API/Dtos/UserForRegisterDto.cs
+++ DattingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DattingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage = "É necessário informar o Nome do Usuário.")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar a senha.")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "A senha deve conter entre 4 e 8 caracteres.")]
         public string Password { get; set; }
     }
 }
