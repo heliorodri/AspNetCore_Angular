1432bfbce0d356f7edefac4c42300a5195d69b97
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2020-04-10 18:00:39 -0300

    Adding register method to the AuthService

--- DattingApp-SPA/src/app/_services/auth.service.ts
+++ DattingApp-SPA/src/app/_services/auth.service.ts
@@ -8,18 +8,23 @@ import { map } from 'rxjs/operators';
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model)
-    .pipe(
-      map((Response: any) => {
-        const user = Response;
-        if (user) {
-          localStorage.setItem('token', user.token);
-        }
-      })
-    );
-}
+  // model is an object that cointains username and password
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model)
+      .pipe(
+        map((Response: any) => {
+          const user = Response;
+          if (user) {
+            localStorage.setItem('token', user.token);
+          }
+        })
+      );
+  }
 
+  // model is an object that cointains username and password
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 }
