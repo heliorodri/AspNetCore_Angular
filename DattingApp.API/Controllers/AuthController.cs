847f90cefcd41f39049f83b9679740cf361682dc
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2020-04-27 22:55:56 -0300

    API Global Exception Handler - Setting up

--- DattingApp.API/Controllers/AuthController.cs
+++ DattingApp.API/Controllers/AuthController.cs
@@ -44,6 +44,8 @@ namespace DattingApp.API.Controllers
         [HttpPost("Login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Computer says no!");
+
             var userFromRepo = await this._repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
