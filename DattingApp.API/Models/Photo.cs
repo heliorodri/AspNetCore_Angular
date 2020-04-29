fb463d527e5920a4c4e69f0c3305b2031f425ce6
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2020-04-29 15:49:28 -0300

    Extending User Model

---
+++ DattingApp.API/Models/Photo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DattingApp.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description  { get; set; }
+        public DateTime Added { get; set; }
+        public bool IsMain { get; set; }
+        
+    }
+}
